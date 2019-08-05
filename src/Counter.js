5fe6cefa16bb0b3559b65de158daf48437c1cb29
Amazing Design <github@amazingdesign.eu>
2019-08-05 09:22:34 +0200

    Cleared and first basic component structure added

---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Counter extends React.Component {
+  render(){
+    return(
+      <div>
+        Counter
+      </div>
+    )
+  }
+}
+export default Counter
---
+++ src/FetchUsers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class FetchUsers extends React.Component {
+  render(){
+    return(
+      <div>
+        FetchUsers
+      </div>
+    )
+  }
+}
+export default FetchUsers
